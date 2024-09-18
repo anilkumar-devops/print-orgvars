1ed92bf4b0270151d67af703c01e22f0ef2b7e5a
anilkumararudra <anilkumararudra@gmail.com>
2024-09-18 18:55:05 +0530

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -2,7 +2,7 @@
 #echo "${ALLMYVARS:1:-1}" | sed "s/\"//g" | sed 's/,$//' | sed '/^[[:space:]]*$/d'
   echo "$ORG_VARS" | jq -r 'to_entries | .[] | .key + "=\"" + .value + "\""' | while read -r env_var; do
     # Append the variable to the temporary environment file
-    echo "export $env_var" >> ENV_FILE
+    #echo "export $env_var" >> ENV_FILE
     
     # Also append the variable to GITHUB_ENV for subsequent steps
     echo "$env_var"
